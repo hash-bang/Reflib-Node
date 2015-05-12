1ddf59131a28b5da205f3c0ab75fd67dc6f66de8
Matt Carter <m@ttcarter.com>
2015-05-12 16:28:48 +1000

    Initial checkin

---
+++ README.md
@@ -0,0 +1,5 @@
+Reflib
+======
+Reference library processing for Node.
+
+This is a ported version of the original [Reflib for PHP](https://github.com/hash-bang/RefLib)
---
+++ index.js
@@ -0,0 +1,27 @@
+var _ = require('lodash');
+var fsPath = require('path');
+
+module.exports = {
+	supported: [
+		{
+			id: 'endnotexml',
+			name: 'EndNote XML file',
+			ext: ['.xml'],
+			driver: require('reflib-endnotexml'),
+		},
+	],
+
+	identify: function(filename) {
+		var ext = fsPath.extName(filename).toLowerCase();
+		var found = _.find(this.supported, function(format) {
+			return _.contains(format.ext, ext);
+		});
+		return found ? found : false;
+	},
+
+	parse: function(format, input) {
+		var supported = _.find(this.supported, {id: format});
+		if (!supported) throw new Error('Format is unsupported: ' + format);
+		return supported.driver.parse(input);
+	},
+};
